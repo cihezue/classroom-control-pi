7d7dfebd88f77b89fcc8615c202487c4f11de147
chaitan18 <chaitan18@yahoo.com>
2016-10-11 13:18:03 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -28,8 +28,7 @@ node default {
   
   # example code for the classroom
   include examples::puppetize
-  
- # notify { "This is the default message from the production environment": }
+  # notify { "This is the default message from the production environment": }
  notify {"Hello World! I am ${::fqdn}":}
  
 }
