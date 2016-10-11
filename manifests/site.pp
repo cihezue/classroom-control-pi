4c8a04d93bc7bc541d533168b4a2eb046c611b84
Meyyam1 <mey.bala@gmail.com>
2016-10-11 13:56:02 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,4 @@
-## site.pp ##
+ ## site.pp ##
 
 # This file (/etc/puppetlabs/puppet/manifests/site.pp) is the main entry point
 # used when an agent connects to a master and asks for an updated configuration.
